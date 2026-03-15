536196c48e2448e402f324dbcfc36af3122810c0
MatthewMc <matthewpmmc@gmail.com>
2026-03-15 16:04:24 +0800

    Fixig random error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -103,7 +103,6 @@ export default function About() {
               width="100%"
               height="352"
               frameBorder="0"
-              allowFullScreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
